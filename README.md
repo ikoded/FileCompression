9ea75e1118fe1f51b8360c0133aaa44d0a3523d5
ikoded <kadinseimet123@gmail.com>
2026-09-09 13:00:32 -0500

    Added encoding and decoding

--- README.md
+++ README.md
@@ -1,12 +1,36 @@
 # FileCompression
 C++ command line tool to compress files using Huffman Coding
 
+## How/Why Huffman Coding
+
+### Why?
+
+Each char in a text file is stored within 1 byte which is 8 bits. To tackle this issue, Huffman Coding uses a min heap and frequency map to decrease the amount of bits stored in a compressed file.
+
+Example: `ABC` can be mapped to A(0) B(10) C(11). `ABC` would be 3 bytes or 24 bits stored uncompressed. However when compressed, it will be stored as 01011 which is only 5 bits.
+
+While 24 -> 5 bits is a very good optimization, you also need to spend a few more bytes in the header of the compressed file for the decoding program to work. This of course defeats the purpose of compressing very small texts like `ABC` but in longer texts it saves much more space as long as it is greater than the header overhead in size.
+
+### How?
+
+Huffman Coding is quite simple logically. The steps are as follows:
+
+1. Get the frequency of each character in a text and store it.
+2. Create a min heap of the frequency, to start at lowest frequency characters.
+3. Grab the left and right leaf nodes (first/second indices of minheap) and create a parent node adding the frequencies pushing it to end of minheap.
+4. Recursively generate the huffman codes based on where the leaf node is, going left append `0` and going right append `1`.
+5. Convert each character in the original text to the respective code generated in step 4.
+
+As you can see, it uses a basic Binary Tree and the Binary Tree Search algorithm.
+
 ## Header Serialization
 
-Binary header needs to tell the decompression program how to decode it
+a binary header needs to tell the decompression program how to decode it.
 
 Use count-based:
 
+```
 4 Bytes (Size of unique characters) + 
 5 Bytes (char + frequency of char int(4 bytes)) + 
 Rest of Bytes (Compressed bits code)
+```
--- compression.cpp
+++ compression.cpp
@@ -38,8 +38,55 @@ void generate_huffman_codes(const std::shared_ptr<Node>& root, const std::string
     }
 }
 
+/*
+
+Encode the text with the generated huffman codes for each character
+
+*/
+std::string encode_text(std::string text, const std::unordered_map<char,std::string> codes){
+    std::string encoded_text = "";
+    for(char c : text){
+        encoded_text.append(codes.at(c));
+    }
+
+    return encoded_text;
+}
+
+/*
+
+The idea for decoding is we always know the first character in the encoded text will be one of the codes 
+[0,n], where n is the length of the code for a character in the codes map.
+
+As we erase from index 0, this means the first character will ALWAYS be a start to one of the codes generated
+for each character.
+
+*/
+std::string decode_text(std::string encoded_text, const std::unordered_map<char,std::string> codes){
+    std::string decoded_text;
+
+    // while greater than 0 because the set of characters get deleted as it finds them
+    while(encoded_text.length() > 0){
+        // go through each code
+        for(auto const& [key,value] : codes){
+            // find length of the source to replace
+            int size_code = value.length();
+            // always starts at 0 index since it erases if found
+            if(encoded_text.substr(0,size_code) == value){
+                // erase the substring
+                encoded_text.erase(0,size_code);
+                // convert the character to a string and append to the decoded text string
+                std::string tempchar(1,key);
+                decoded_text.append(tempchar);
+                // no longer need to look at other codes
+                break;
+            }
+        }
+    }
+    return decoded_text;
+}
+
 int main() {
-    std::string text = "AABCBAD";
+    std::string text = "This is a file compression using the Huffman Coding algorithm.";
     std::unordered_map<char,int> frequencies;
 
     // Grab frequency of characters in text
@@ -81,10 +128,13 @@ int main() {
     std::unordered_map<char, std::string> codes;
     generate_huffman_codes(root,"",codes);
 
-    // print values for now, in future will add decoder as well as serialize through headers
-    for(auto const& [key,value] : codes){
-        std::cout << key << " " << value << std::endl;
-    }
+    std::cout << "Original text: " << text << std::endl;
+
+    std::string encoded_text = encode_text(text,codes);
+    std::cout << "Encoded text: " << encoded_text << std::endl;
+
+    std::string decoded_text = decode_text(encoded_text,codes);
+    std::cout << "Decoded text: " << decoded_text << std::endl;
 
 
     return 0;
