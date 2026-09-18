b95ad324e78a21dfc95d431e85446099ea77017e
ikoded <kadinseimet123@gmail.com>
2026-09-18 11:46:19 -0500

    Add code to headers

--- include/File.h
+++ include/File.h
@@ -4,11 +4,13 @@
 #include <stdexcept>
 #include <iostream>
 #include <filesystem>
+#include <unordered_map>
+#include "HuffmanCode.h"
 
 class File {
     public:
         std::string read_file(std::string filename);
-        void compress_file(std::string filename, std::string encoded_text);
+        void compress_file(std::string filename, std::string encoded_text, std::unordered_map<char, std::string> codes);
         std::string decompress_file(std::string filename);
         void output_decoded_content(std::string filename, std::string file_content);
         void compare_sizes(std::string filename);
