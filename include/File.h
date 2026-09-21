f4d91735273a0f948ad16e284964f31219cb20fa
Koder <45540379+ikoded@users.noreply.github.com>
2026-09-21 18:34:18 -0500

    Isolate and convert to CLI (#7)
    * Isolate and convert to CLI
    
    * Fix README

--- include/File.h
+++ include/File.h
@@ -5,13 +5,15 @@
 #include <iostream>
 #include <filesystem>
 #include <unordered_map>
+#include <vector>
 #include "HuffmanCode.h"
 
 class File {
     public:
+        std::vector<std::string> get_files(std::string dir);
         std::string read_file(std::string filename);
         void compress_file(std::string filename, std::string encoded_text, std::unordered_map<char, std::string> codes);
-        std::string decompress_file(std::string filename);
+        void decompress_files();
         void output_decoded_content(std::string filename, std::string file_content);
         void compare_sizes(std::string filename);
 };
